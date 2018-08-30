6c94d4e43fa9142393c1b733767476b329204720
Henner Harnisch <Henner.Harnisch@web.de>
2018-08-30 14:52:04 +0200

    HHHH

--- dist/configurations/config.js
+++ dist/configurations/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: "heymynameismohamedaymen",
-    port: 8080
+    port: 8888
 };
 //# sourceMappingURL=config.js.map
--- src/configurations/config.ts
+++ src/configurations/config.ts
@@ -1,4 +1,4 @@
 module.exports = {
     secret : "heymynameismohamedaymen",
-    port: 8080
+    port: 8888
 };
